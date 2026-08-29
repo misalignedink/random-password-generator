23d4db50b89acd076e3cb0a350baf022cb54480f
misalignedink <misalignedink@gmail.com>
2026-08-29 15:37:53 +0530

    little tweaks cutie

--- main.py
+++ main.py
@@ -11,36 +11,41 @@ print("""
               
               """)
 
-length = int(input("Enter your password length (e.g. 8): "))
-
-def letters_numbers():
-    total = string.ascii_letters + string.digits
-    password = "".join(random.sample(total, length))
-    print("")
-    print("Your Password is: ", password)
-    print("")
-
-def letters_numbers_punctuations():
-    total = string.ascii_letters + string.digits + string.punctuation
-    password = "".join(random.sample(total, length))
-    print("")
-    print("Your Password is: ", password)
-    print("")
-    
-
 def main():
+
+    def letters_numbers():
+        total = string.ascii_letters + string.digits
+        password = "".join(random.sample(total, length))
+        print("")
+        print("Your Password is: ", password)
+        print("")
+
+    def letters_numbers_punctuations():
+        total = string.ascii_letters + string.digits + string.punctuation
+        password = "".join(random.sample(total, length))
+        print("")
+        print("Your Password is: ", password)
+        print("")
+    
     while True:
+        print("")
+
         print("\nChoose your password combination: ")
         print(" 1. Letters + Numbers")
         print(" 2. Letters + Numbers + Special Symbols")
         print(" 3. Exit")
 
         choice = input("\nChoose an Option (e.g. 1): ")
+        
 
         if choice == "1":
+            length = int(input("\nEnter your password length (e.g. 8): "))
             letters_numbers()
+            input("\nPress Enter to continue...")
         elif choice == "2":
+            length = int(input("\nEnter your password length (e.g. 8): "))
             letters_numbers_punctuations()
+            input("\nPress Enter to continue...")
         elif choice == "3":
             print("")
             print("👋 Exiting... Have a great day!")
@@ -48,8 +53,7 @@ def main():
             sys.exit()
         else:
             print("Invalid Choice\n")
-
-        break   
+            input("\nPress Enter to continue...")          
 
 if __name__ == "__main__":
     main()
